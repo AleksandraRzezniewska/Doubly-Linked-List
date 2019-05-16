594744c5fd933c5fb55428316cf7e51b673b85d7
Aleksandra Rzezniewska <o.rzezniewska@gmail.com>
2019-05-16 14:40:38 +0200

    First commit

---
+++ BidirectionalList/DoubleLinkedList.cs
@@ -0,0 +1,76 @@
+﻿namespace BidirectionalList
+{
+    internal class DoubleLinkedList<T>
+    {
+        internal Node<T> head;
+
+        public void InsertFront(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+
+                return;
+            }
+
+            node.next = head;
+            head.previous = node;
+            head = node;
+        }
+
+        public void InsertEnd(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+
+                return;
+            }
+
+            Node<T> previousNode = GetPreviousNode();
+            previousNode.next = node;
+            node.previous = previousNode;
+        }
+
+        public Node<T> GetPreviousNode()
+        {
+            Node<T> node = head;
+
+            while (node.next != null)
+            {
+                node = node.next;
+            }
+
+            return node;
+        }
+
+        public void InsertAfter(Node<T> prevNode, T data)
+        {
+            if (prevNode == null)
+            {
+                System.Console.WriteLine("Previous node is null");
+                return;
+            }
+
+            Node<T> node = new Node<T>(data);
+            node.next = prevNode.next;
+            prevNode.next = node;
+            node.previous = prevNode;
+        }
+
+        public void PrintList()
+        {
+            Node<T> node = head;
+
+            while (node != null)
+            {
+                System.Console.Write(node.data);
+                node = node.next;
+            }
+        }
+    }
+}
---
+++ BidirectionalList/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace BidirectionalList
+{
+    internal class Node<T>
+    {
+        internal T data;
+        internal Node<T> previous;
+        internal Node<T> next;
+
+        public Node(T data)
+        {
+            this.data = data;
+            previous = null;
+            next = null;
+        }
+    }
+}
